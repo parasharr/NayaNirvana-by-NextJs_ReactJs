28c9eb22f11d390c779ddf3e10024285037260e3
PranjeetGoswami <prgoswami2020@gmail.com>
2025-03-02 11:54:02 +0530

    fixes issues

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -5,19 +5,13 @@ import CustomizeProducts from "../components/CustomizeProducts";
 import Add from "../components/Add";
 import DOMPurify from "isomorphic-dompurify";
 
-// Define the PageProps type
-type PageProps = {
-  params: {
-    slug: string;
-  };
-};
-
-const SinglePage = async ({ params }: PageProps) => {
+const SinglePage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const resolvedParams = await params;
   const wixClient = await wixClientServer();
 
   const products = await wixClient.products
     .queryProducts()
-    .eq("slug", params.slug)
+    .eq("slug", resolvedParams.slug)
     .find();
 
   if (!products.items[0]) {
@@ -76,11 +70,7 @@ const SinglePage = async ({ params }: PageProps) => {
         {product.additionalInfoSections?.map((section: any) => (
           <div className="text-sm" key={section.title}>
             <h4 className="font-medium mb-4">{section.title}</h4>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: section.description,
-              }}
-            ></p>
+            <p dangerouslySetInnerHTML={{ __html: section.description }}></p>
           </div>
         ))}
         <div className="h-[2px] bg-gray-100" />
