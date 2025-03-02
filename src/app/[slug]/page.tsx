4d6464ba7f78b99aef3403c153aa300b545b2d20
PranjeetGoswami <prgoswami2020@gmail.com>
2025-03-02 11:48:25 +0530

    fixes issues

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -76,7 +76,11 @@ const SinglePage = async ({ params }: PageProps) => {
         {product.additionalInfoSections?.map((section: any) => (
           <div className="text-sm" key={section.title}>
             <h4 className="font-medium mb-4">{section.title}</h4>
-            <p dangerouslySetInnerHTML={{ __html: section.description }}></p>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: section.description,
+              }}
+            ></p>
           </div>
         ))}
         <div className="h-[2px] bg-gray-100" />
